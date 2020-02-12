18f12a87577f1b87cb8220785c669544a7bf22a8
Tom Dolidon <Tom.Dolidon@etu.univ-grenoble-alpes.fr>
2020-02-12 10:40:28 +0100

    User attribute firstName & lastName + first test

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -1,26 +1,33 @@
 <?php
-
-
 namespace App\Tests\Entity;
 
-
-
+use PHPUnit\Framework\TestCase;
 use App\Entity\User;
 
 final class UserTest extends TestCase
 {
 
-    public function testNewUser()
+    protected $user;
+
+    public function setUp(): void
     {
-        $user = new User();
-        $this->assertInstanceOf(User::class, $user);
-        $this->assertNull($user->getId());
+        $this->user = new User();
     }
 
-    public function testUserName() {
-        $user = new User();
-        $user->setName("Adil");
-        $this->assertEquals("Adil", $user->getName());
+    public function testNewUser()
+    {
+        $this->assertInstanceOf(User::class, $this->user);
+        $this->assertNull($this->user->getId());
+    }
+
+    public function testUserFirstName() {
+        $this->user->setFirstName("Adil");
+        $this->assertEquals("Adil", $this->user->getFirstName());
+    }
+
+    public function testUserLastName() {
+        $this->user->setLastName("Sajide");
+        $this->assertEquals("Sajide", $this->user->getLastName());
     }
 
 
